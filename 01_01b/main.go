3d030b849c3d90174e1571b257116b33d500b5a1
Raisa Rahman <124052639+raisa-rahman@users.noreply.github.com>
2024-07-26 14:01:17 +0000

    "Sleeps" until your birthday

--- 01_01b/main.go
+++ 01_01b/main.go
@@ -10,18 +10,31 @@ var expectedFormat = "2006-01-02"
 
 // parseTime validates and parses a given date string.
 func parseTime(target string) time.Time {
-	panic("NOT IMPLEMENTED")
+	// We begin by parsing the time given the expected format.
+	pt, err := time.Parse(expectedFormat, target)
+	// Add an 'if' check for a non-nil parsing error or if time.Now is after the parsed time.
+	if err != nil || time.Now().After(pt) {
+		// In the error case, we will terminate the program.
+		log.Fatal("invalid target date: ", target)
+	}
+
+	return pt
 }
 
 // calcSleeps returns the number of sleeps until the target.
 func calcSleeps(target time.Time) float64 {
-	panic("NOT IMPLEMENTED")
+	// Use time.Until to get the duration until the target time.
+	// Convert the duration to hours and then divide by 24 to get the number of days.
+	return time.Until(target).Hours() / 24
 }
 
 func main() {
+	// Read the target birthday as a command line argument.
 	bday := flag.String("bday", "", "Your next bday in YYYY-MM-DD format")
 	flag.Parse()
+	// Parse the target birthday.
 	target := parseTime(*bday)
+	// Log the number of sleeps until the target birthday.
 	log.Printf("You have %d sleeps until your birthday. Hurray!",
 		int(calcSleeps(target)))
 }
